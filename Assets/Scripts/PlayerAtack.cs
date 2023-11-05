5bde0b9db587f46208638e52966c5e432694b36d
WildCup <hagelhubert@gmail.com>
2023-11-05 10:55:00 +0100

    init base player range attack, separate atack input from attack mechanics

---
+++ Assets/Scripts/PlayerAtack.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using UnityEngine;
+using System;
+
+public class PlayerAtack : MonoBehaviour
+{
+    [SerializeField] int dmg = 1;
+    [SerializeField] float force = 2f;
+    [SerializeField] float range = .8f;
+    [SerializeField] private LayerMask enemyLayer;
+    [SerializeField] GameObject rock;
+
+    public void Attack()
+    {
+        var enemies = Physics2D.OverlapCircleAll(transform.position, range, enemyLayer).Where(e => e.CompareTag("Enemy"));
+        foreach (var enemy in enemies)
+        {
+            enemy.GetComponent<Health>().Damage(dmg);
+        }
+    }
+
+    public void Throw(float powerPercentage)
+    {
+        if (powerPercentage < 0 || powerPercentage > 1) throw new ArgumentOutOfRangeException("you can only throw with percentage power between 0% and 100%!!");
+
+        var target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        target.z = 0f;
+        var direction = target - transform.position;
+
+        var instangce = Instantiate(rock, transform.position, Quaternion.identity);
+        var script = instangce.GetComponent<Rock>();
+        script.Lunch(direction, force * powerPercentage);
+    }
+
+    void OnDrawGizmos() //show attack range
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,49 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Rock : MonoBehaviour
+{
+    [SerializeField] int dmg = 1;
+    [SerializeField] float range = .8f;
+    [SerializeField] LayerMask enemyLayer;
+    Rigidbody2D rb;
+
+    [SerializeField] UnityEvent destroyedEvent;
+    [SerializeField] UnityEvent createdEvent;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        createdEvent.Invoke();
+    }
+
+    public void Lunch(Vector2 direction, float force)
+    {
+        rb.velocity = direction * force;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Wall") && !other.CompareTag("Enemy")) return;
+        Explode();
+    }
+
+    void Explode()
+    {
+        Debug.Log($"EXPLOSION");
+        var enemies = Physics2D.OverlapCircleAll(transform.position, range, enemyLayer).Where(e => e.CompareTag("Enemy"));
+        foreach (var enemy in enemies)
+        {
+            enemy.GetComponent<Health>().Damage(dmg);  //enemy.GetComponent<EnemyAI>().Chase(this.position);
+        }
+        destroyedEvent.Invoke();
+        Destroy(gameObject);
+    }
+    
+    void OnDrawGizmos() //show explode range
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
